d8bdfa7a0c89c6342c06c5e47a74d19f646f0f04
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-07-28 20:29:02 +0530

    python movie list page

---
+++ hello_app/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def print_hello(request):
+    movie_data = {
+        "movies": [
+            {
+                "title": "The Godfather",
+                "year": 1972,
+                "summary": "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+                "success": False,
+            },
+            {
+                "title": "The Shawshank Redemption",
+                "year": 1994,
+                "summary": "a guy who escape jail",
+                "success": True,
+            },
+            {
+                "title": "Schindler's List",
+                "year": 1993,
+                "summary": "get to watch he movie wee",
+                "success": True,
+            },
+            {
+                "title": "Raging Bull",
+                "year": 1980,
+                "summary": "a guy who is a boxer",
+                "success": False,
+            },
+            {
+                "title": "Casablanca",
+                "year": 1942,
+                "summary": "a guy who is a boxer",
+                "success": True,
+            },
+        ]
+    }
+    return render(request, "hello.html", movie_data)
